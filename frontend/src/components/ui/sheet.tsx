a86d516587ae6aee7ab1ebb752683cf57ed84551
Angel Esteban <angelestebanpc@gmail.com>
2025-03-02 21:30:57 -0400

    Cambio en el sheet.tsx

--- frontend/src/components/ui/sheet.tsx
+++ frontend/src/components/ui/sheet.tsx
@@ -68,8 +68,10 @@ function SheetContent({
         )}
         {...props}
       >
-        {/* Título de la hoja (requerido para accesibilidad) */}
-        <SheetTitle className="sr-only">Título de la hoja</SheetTitle>
+        <SheetHeader>
+          <SheetTitle>Título de la hoja</SheetTitle>
+          <SheetDescription>Descripción de la hoja</SheetDescription>
+        </SheetHeader>
         {children}
         <SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none">
           <XIcon className="size-4" />
